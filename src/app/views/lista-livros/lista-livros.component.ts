1cf34739933e875fab385384a789a719be4b4d68
gtoyo <ggabrieltoyo@gmail.com>
2023-11-30 18:30:06 -0300

    Implementando a conexão com API de livros do Google + Ajustes de template

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Item, Livro, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: LivroVolumeInfo[];
+  campoBusca: string = '';
+  subscription: Subscription;
+  livro: Livro;
 
-  constructor() { }
+  constructor(private service: LivroService) { }
+
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (result) => {
+        this.listaLivros = this.livrosResultadoParaLivros(result);
+      },
+      error: erro => console.error(erro),
+    });
+  }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
-
-
-
