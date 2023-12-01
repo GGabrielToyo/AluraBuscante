6b3f5698d9189f239fbda9c34574d7c3c88eb816
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2023-12-01 18:14:09 -0300

    tratando erros, melhorando usabilidade.

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
-import { Item, LivrosResultado } from '../models/interfaces';
+import { Observable  } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +11,10 @@ export class LivroService {
 
   constructor(private http: HttpClient) { }
 
-  public buscar(valorDigitado): Observable<Item[]> {
+  public buscar(valorDigitado): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      map(reusult => reusult.items)
-    );
+
+    return this.http.get<LivrosResultado>(this.API, { params });
   }
+  
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,40 +1,44 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Item, Livro, LivrosResultado } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { EMPTY, catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA: number = 300;
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  listaLivros: LivroVolumeInfo[];
-  campoBusca: string = '';
-  subscription: Subscription;
-  livro: Livro;
+  campoBusca = new FormControl();
+  mensagemErro: string = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
 
-  buscarLivros() {
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (result) => {
-        this.listaLivros = this.livrosResultadoParaLivros(result);
-      },
-      error: erro => console.error(erro),
-    });
-  }
+
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    map(reusult => reusult.items ?? []),
+    map(items => this.livrosResultadoParaLivros(items)),
+    catchError(() => {
+      this.mensagemErro = "Ops, ocorreu um erro. Recarregue a aplicação.";
+      return EMPTY;
+    })
+  );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
       return new LivroVolumeInfo(item);
     });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 
 }
